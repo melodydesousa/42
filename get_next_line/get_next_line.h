3c9bd732c381eb77e30d6247b66dbb37732f8199
melodydesousa <97676133+melodydesousa@users.noreply.github.com>
2022-05-17 14:34:00 +0200

    Add files via upload

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mede-sou <mede-sou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 12:11:37 by mede-sou          #+#    #+#             */
-/*   Updated: 2022/05/17 12:37:37 by mede-sou         ###   ########.fr       */
+/*   Updated: 2022/05/17 14:24:12 by mede-sou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <stdlib.h>
 # include <string.h>
+# include <stdio.h>
 
 char	*get_next_line(int fd);
 
