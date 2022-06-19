3a14d58096596d3138f0288ac847fe516fbc4f8b
melodydesousa <97676133+melodydesousa@users.noreply.github.com>
2022-06-19 21:52:16 +0200

    Update camel_to_snake.c

--- exam02/level2/camel_to_snake.c
+++ exam02/level2/camel_to_snake.c
@@ -1,47 +1,22 @@
 #include <unistd.h>
-#include <stdlib.h>
-
-int main (int argc, char **argv)
+int main (int ac, char **av)
 {
-	int i;
-	int j;
-	int count;
-	char *s;
-	
-	i = 0;
-	j = 0;
-	count = 0;
-	if (argc == 2)
-	{
-		while (argv[1][i])
-		{
-			if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
-				count++;
-			i++;
-		}
-		s = malloc(sizeof(char) * (count + i + 1));
-		i = 0;
-		while (argv[1][i])
-		{
-			if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
-			{
-				s[j] = '_';
-				j++;
-				s[j] = argv[1][i] + 32;
-			}
-			else 
-				s[j] = argv[1][i];
-			j++;
-			i++;
-		}
-		j = 0;
-		while (s[j])
-		{
-			write(1, &s[j], 1);
-			j++;
-		}
-	}
-	write(1, "\n", 1);
-	free(s);
-	return (0);
+    int i = 0;
+    if (ac == 2)
+    {
+        while (av[1][i])
+        {
+            if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+            {
+                write (1, "_", 1);
+                av[1][i] = av[1][i] + 32;
+                write (1, &av[1][i], 1);
+            }
+            else if (av[1][i] >= 'a' && av[1][i] <= 'z')
+                write (1, &av[1][i], 1);
+            i++;
+        }
+    }
+    write (1, "\n", 1);
+    return (0);
 }
