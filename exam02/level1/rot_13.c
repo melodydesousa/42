a3e405ff962df4e79c41f1084bcbcc7bf7de19fe
melodydesousa <97676133+melodydesousa@users.noreply.github.com>
2022-06-20 19:18:23 +0200

    Update rot_13.c

--- exam02/level1/rot_13.c
+++ exam02/level1/rot_13.c
@@ -4,38 +4,21 @@
 int main (int arc, char **arv)
 {
 	int i;
-	int j;
-	char s1[26] = "abcdefghijklmnopqrstuvwxyz";
-	char s2[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 	i = 0;
 	if (arc == 2)
 	{
 		while (arv[1][i])
 		{
-			j = 0;
 			if (arv[1][i] >= 'a' && arv[1][i] <= 'm')
-			{
-				j = arv[1][i] - 'a' + 13;
-				write (1, &s1[j], 1);
-			}
+				arv[1][i] = arv[1][i] - 'a' + 13;
 			else if (arv[1][i] >= 'n' && arv[1][i] <= 'z')
-			{
-				j = arv[1][i] - 'a' - 13;
-				write (1, &s1[j], 1);
-			}
+				arv[1][i] = arv[1][i] - 'a' - 13;
 			else if (arv[1][i] >= 'A' && arv[1][i] <= 'M')
-			{
-				j = arv[1][i] - 'A' + 13;
-				write (1, &s2[j], 1);
-			}
+				arv[1][i] = arv[1][i] - 'A' + 13;
 			else if (arv[1][i] >= 'N' && arv[1][i] <= 'Z')
-			{
-				j = arv[1][i] - 'A' - 13;
-				write (1, &s2[j], 1);
-			}
-			else
-				write(1, &arv[1][i], 1);
+				arv[1][i] = arv[1][i] - 'A' - 13;
+			write(1, &arv[1][i], 1);
 			i++;
 		}
 	}
