1d11c6927567685cf84297ff6e8f933bd53bba47
melodydesousa <97676133+melodydesousa@users.noreply.github.com>
2022-06-20 19:34:23 +0200

    Update do_op.c

--- exam02/level2/do_op.c
+++ exam02/level2/do_op.c
@@ -6,14 +6,12 @@ int main (int argc, char **argv)
 {
 	int i;
 	int j;
-	char op;
 	int n;
 
 	if (argc == 4)
 	{
 		i = atoi(argv[1]);
 		j = atoi(argv[3]);
-		op = argv[2][0];
 		if (argv[2][0] == '-')
 			n = i - j;
 		else if (argv[2][0] == '+')
