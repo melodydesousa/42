0c520761e83019ed0e0e04f8d449db96025d71dd
melodydesousa <97676133+melodydesousa@users.noreply.github.com>
2022-08-18 15:07:45 +0200

    Add files via upload

---
+++ sosolong/so_long.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mede-sou <mede-sou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/17 10:54:57 by mede-sou          #+#    #+#             */
+/*   Updated: 2022/08/17 18:36:57 by mede-sou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "mlx_linux/mlx.h"
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+#define W 119
+#define A 97
+#define S 115
+#define D 100
+#define ESC 65307
+
+#endif
