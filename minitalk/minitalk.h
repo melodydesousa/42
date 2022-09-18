beefbe3a7377d9f8f4cd88a7e6603d5374764ad0
melodydesousa <97676133+melodydesousa@users.noreply.github.com>
2022-09-18 19:28:05 +0200

    Add files via upload
    minitalk

---
+++ minitalk/minitalk.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mede-sou <mede-sou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/08 12:23:38 by mede-sou          #+#    #+#             */
+/*   Updated: 2022/08/11 13:39:35 by mede-sou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include <sys/types.h>
+# include "libft/libft.h"
+# include "libft/printf/ft_printf.h"
+# include "libft/get_next_line/get_next_line.h"
+
+#endif
